87a75c62b1c9b3a43971a98f6e21b8bdd10df054
ravi <sama369.rk@gmail.com>
2023-10-04 12:48:40 +0530

    portfolio part completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './compontents/Header/Header'
+import Portfolio from './compontents/Portfolio/Portfolio'
+
+const App = () => {
+  return (
+    <>
+    <Portfolio/>
+  <Header/>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/compontents/Portfolio/Portfolio.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Portfolio.scss'
+const Portfolio = () => {
+  return (
+    <>
+      <div className="portfolio">
+        <div className="portfolio_text">
+          <h3 className='port'>
+            <span className='port_text_hover' >P</span>
+            <span className='port_text_hover' >o</span>
+            <span className='port_text_hover' >r</span>
+            <span className='port_text_hover' >t</span>
+          </h3>
+
+          <h3 className='folio'>
+            <span className='port_text_hover2' >f</span>
+            <span className='port_text_hover2' >o</span>
+            <span className='port_text_hover2' >l</span>
+            <span className='port_text_hover2' >i</span>
+            <span className='port_text_hover2' >o</span>
+
+
+
+
+          </h3>
+        </div>
+
+
+
+
+
+
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default Portfolio
