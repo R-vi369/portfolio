a71e4e9d013a660d5f570e24fec533169dfdf5e0
ravi <sama369.rk@gmail.com>
2023-10-13 21:47:34 +0530

    nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,38 +1,44 @@
 import React from 'react'
 import '../styles/Header.scss'
 import { Link } from 'react-router-dom'
-import { AiOutlineHome ,AiOutlineBulb} from 'react-icons/ai'
-import {VscTools} from 'react-icons/vsc'
-import {RiContactsLine} from 'react-icons/ri'
-import {LuContact2} from 'react-icons/lu'
+import { AiOutlineHome, AiOutlineBulb } from 'react-icons/ai'
+import { VscTools } from 'react-icons/vsc'
+import { RiContactsLine } from 'react-icons/ri'
+import { LuContact2 } from 'react-icons/lu'
+
 const Header = () => {
   return (
     <>
 
-      <div className="header">
-        <Link  to='/Home' className='link active' >
-         
+
+      <div className="header" >
+
+        <Link to='/Home' className='link active' >
+
           <AiOutlineHome />
         </Link>
         <Link to='/about' className='link'>
           <RiContactsLine />
         </Link>
-        <Link to='/skills'  className='link'>
+        <Link to='/skills' className='link'>
           <VscTools />
         </Link>
         <Link to='/Projects' className='link'>
-          <AiOutlineBulb/>
+          <AiOutlineBulb />
         </Link>
         <Link to='/Home' className='link'>
           <AiOutlineHome />
         </Link>
         <Link to='/contact' className='link'>
-          <LuContact2/>
+          <LuContact2 />
         </Link>
       </div>
-  
+
     </>
   )
 }
 
 export default Header
+
+
+
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,22 +3,41 @@ import '../styles/Home.scss'
 import Home2 from './Home2'
 import pro_img from '../assets/profile.png'
 import  Footer from './Footer'
+import Typed from 'typed.js';
 
 
 const Home = () => {
+    // typed js
+  const el = React.useRef(null);
+
+
+
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: ['A passionate web developer', 'front end developer.', 'currently leanring Backend developement.'],
+      typeSpeed: 150,
+   
+      loop: true,
+    });
+
+
+  
+  });
 
   return (
     <>
       <div className="home">
+     
+
         <div className="home_left">
         <h3>Hola, I'm</h3>
               <h1>Ravi</h1>
-              <p>A passionate web developer | currently leanring Backend developement | front end developer | </p>
+              <p> <span ref={el} /> </p>
              
         </div>
           
           <div className="home_img">
-            <img src={pro_img} alt="" />
+            <img className='idd' src={pro_img} alt="" />
           </div>
       </div>
       <Home2/>
@@ -29,5 +48,6 @@ const Home = () => {
 }
 
 export default Home
+
 
 
